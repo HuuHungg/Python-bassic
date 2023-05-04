90e828acd5d26ebb734edefef594dadfca6e8eaa
Huu Hung <huuhungnguyen2002@gmail.com>
2023-05-04 21:09:27 +0700

    add file modifile

--- index.py
+++ index.py
@@ -1,18 +1,64 @@
-my_list = ["日本語", "英語", "明日"]
+# Giới hạn bởi các cặp ngoặc vuông []
+# Các phần tử của list cách nhau bởi dấu ,
+# list chứa mọi giá trị của Python
+# và bao gồm chính nó 
 
-print(my_list[0])
-print(my_list[1])
-print(my_list[2])
+a = [ 1, "list", 2 , 5, "Convert", ["interface", "Name",], ["course Python"]]
+print(a)
 
-fruits = ["apple", "banana", "cherry"]
-fruits[1] = "yushing"
+# list rỗng
+b = [i for i in range(30)]
+print(b)
 
-print(fruits)
+c = [yushing for yushing in ("TypeScript")]
+print(c)
 
-English = ["English", "Japanese", "Spans"]
-English.append("Chinese")
-print(English)
+d = [[n, n *1, n*2] for n in range(1,5)]
+print(d)
 
-CourseIt = ["Iphone", "Mac", "Interface", "Yushing"]
-CourseIt.reverse()
-print(CourseIt)
+a = [1,2]
+a += ['one', 'two']
+print(a)
+
+j = [3,4]
+b = 'Kteam' in a
+print(b)
+
+a = [1,2, 'a', 'b', 'c', [3,4]]
+a[1] = 'guenfufun'
+b = a[1]
+print(b)
+print(a)
+
+a = [[1,2,3], [4,5,6], [7,8,9]]
+print(a[0][-1])
+print(a[1][1])
+print(a[2][2])
+
+l = [1,23,4,5,7]
+p = l
+print(l)
+print(p)
+
+p[0] = 'kteam'
+print(p)
+print(l)
+
+a = [[1,2,3], [4,5,6]]
+b = a
+print(a)
+print(b)
+
+print("======")
+
+r = [[1,2,3,45,6], [7,8,9,10]]
+t = list(r)
+
+print(r)
+print(t)
+t[0] = 'Yushing'
+print(r)
+print(t)
+
+
+

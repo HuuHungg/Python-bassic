685d80a106b84faec10da5c50dc285fd497a1c17
Huu Hung <huuhungnguyen2002@gmail.com>
2023-05-05 11:52:17 +0700

    add file modifile

--- index.py
+++ index.py
@@ -1,112 +1,37 @@
-# Giới hạn bởi các cặp ngoặc vuông []
-# Các phần tử của list cách nhau bởi dấu ,
-# list chứa mọi giá trị của Python
-# và bao gồm chính nó 
+a = 'Kteam'
+b = id(a)
 
-a = [ 1, "list", 2 , 5, "Convert", ["interface", "Name",], ["course Python"]]
-print(a)
-
-# list rỗng
-b = [i for i in range(30)]
 print(b)
+print(id("Kteam"))
 
-c = [yushing for yushing in ("TypeScript")]
-print(c)
+n = 69
+print(n)
+print(n * 2)
+print(n.__mul__(2))
 
-d = [[n, n *1, n*2] for n in range(1,5)]
-print(d)
+s_1 = "HowKteam" 
+s_2 = 'Free education'
 
-a = [1,2]
-a += ['one', 'two']
-print(a)
+s_1 = s_1 + 'Python'
+s_2 += 'Python'
 
-j = [3,4]
-b = 'Kteam' in a
-print(b)
-
-a = [1,2, 'a', 'b', 'c', [3,4]]
-a[1] = 'guenfufun'
-b = a[1]
-print(b)
-print(a)
-
-a = [[1,2,3], [4,5,6], [7,8,9]]
-print(a[0][-1])
-print(a[1][1])
-print(a[2][2])
-
-l = [1,23,4,5,7]
-p = l
-print(l)
-print(p)
-
-p[0] = 'kteam'
-print(p)
-print(l)
-
-a = [[1,2,3], [4,5,6]]
-b = a
-print(a)
-print(b)
-
-print("======")
-
-r = [[1,2,3,45,6], [7,8,9,10]]
-t = list(r)
-
-print(r)
-print(t)
-t[0] = 'Yushing'
-print(r)
-print(t)
-
-
-# Hàm count đếm xem số lần xuất hiện của 1 phần tử là bao nhiêu lần 
-a = [1,1,2,2,2,3,4,5,6,7]
-c = a.count(1) 
-print(c)
-
-money = [60]
-money_all = money
-
-print(money)
-print(money_all)
-
-print ("======")
-
-money_all.clear()
-
-print(money)
-print(money_all)
-
-a = [1,2,3,4,5]
-a.append([6,7])
-print(a)
-
-a.extend([6,7])
-print(a)
-
-#insert (Thêm phần tử, vào vị trí) 
-a = [1,2,3]
-print(a)
-a.insert(4,4)
-print(a)
+print(id(s_1))
+print(id(s_2))
 
 print("=====")
 
-#Pop Lấy ra phần tử và trả về phần tử đã lấy
-a = [10,11,12]
-c = a.pop(2)
+s_3 = [1,2]
+print(id(s_3))
 
-print(a)
-print(c)
+s_3 = s_3.__add__([3,4])
+print(id(s_3))
 
-n = [1,2,8,9,4,5]
-n.remove(2)
-print(n)
-n.reverse() # Đảo ngược lại
-print(n)
+print("====")
+s_4 = [1,2,3]
+s_4.append(4)
 
-# sort sắp xếp từ bé đến lớn và đảo ngược theo key
-n.sort()
-print(n)
+print(s_4)
+
+s_5 = (1,2,3,4,5)
+s_5 += (6,7)
+print(s_5)

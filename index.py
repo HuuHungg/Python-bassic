657c6ec7ab8c23438f9de5af418d563077f8f417
Huu Hung <huuhungnguyen2002@gmail.com>
2023-05-09 11:11:48 +0700

    add file modifile

--- index.py
+++ index.py
@@ -1,18 +1,23 @@
-file_object = open('Yushing.txt')
-data = file_object.read()
-file_object.close()
-file_object = open('Yushing.txt')
-data2 = file_object.read()
-print(data)
-print(" ===== ")
-print(data2)
+itera = (x for x in range(3))
+print(itera)
+print(sum(itera,2))
+print(max (9,5,25,6))
+print(max([], default = "Yushing"))
 
-# Phương thức readline là đọc từng dòng
-data = file_object.readline()
-data2 = file_object.readline()
-file_object.close()
-print(data)
-print(data2)
+itera2 = [1,2,7,9,6,4,3]
+print(itera2)
+print(sorted(itera2))
+print(sorted(itera2, reverse=True))
 
+numbers = [1,2,3,4,5]
+i = 0
+while i < len(numbers):
+    print(numbers[i])
+    i += 1
 
+string = 'Hello, world in the Python'
 
+for char in string:
+    print(char)
+
+print(123, [4,5,6], 'Kteam')

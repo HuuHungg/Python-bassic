12a21fcdf29f7863428c037dfb34b42a259f6feb
Huu Hung <huuhungnguyen2002@gmail.com>
2023-05-12 15:42:57 +0700

    add file modifile

--- index.py
+++ index.py
@@ -1,56 +1,47 @@
-age = 18 
-if age >= 18:
-    print("You can drive motobike")
+k = 5
+while k > 0:
+    print('K = ', k)
+    k -= 1
+
+s = 'How Kteam'
+idx = 0 # Vị trí bắt đầu muốn xử lý chuỗi
+length = len(s) # Lấy độ dài chuỗi làm mốc kết thúc
+while idx < length:
+    print(idx, 'stands for', s[idx])
+    idx += 1
+
+# BREAK 
+five_even_numbers = []
+k_number = 1
+
+while True:
+    if k_number % 2 == 0:
+        five_even_numbers.append(k_number)
+        if len(five_even_numbers) >= 5:
+            break
+    k_number += 1
+
+print(five_even_numbers)
+print(k_number)
+
+print("=======")
+
+# Continue
+k_number2 = 0
+while k_number2 < 10:
+    k_number2 += 1
+    if k_number2 % 2 == 0: # Nếu in ra những số chẵn thì nó sẽ bỏ qua và tiếp tục in 
+        continue
+    print(k_number2, "is odd number")
+
+print(k_number2)
+
+k = 0
+while k < 3:
+    print('value key is of ',k)
+    k += 1
+    if k == 2:
+        break
 else: 
-    print("You can't drive motobike")
-
-num =  int(input("Nhập số nguyên: "))
-
-if num > 0:
-    print("Số này là số dương")
-elif num == 0:
-    print('Số này là số không')
-else:
-    print('Số này là số âm' )
-
-numbers = [1,2,3,4,5,6,7,8,9,10]
-
-for num in numbers:
-    if num % 2 == 0:
-        print(num, "Even number")
-    else:
-        print(num, "odd")
-
-
-a = 0
-b = 0
-if a - 1 < 0:
-    print('a smaller than 1')
-    if b - 1 < 0:
-        print('b bigger than 1')
-
-# Cả khối trên là một blog của if 
-
-
-a = 3
-if  a - 1 < 0: # False, tiếp tục    
-    print('a is less than 1')
-elif a - 2 < 0:
-    print('a is less than 2')
-elif a - 3 < 0:
-    print('a is less than 3')
-elif a - 4 < 0:
-    print('a is less than 4')
-elif a - 5 < 0:
-    print('a is less than 5')
-else:
-    print("I don't know")
-
-
-b = 4
-if b - 1 < 0:
-    print('b is less than 1')
-else: 
-    print('a is less than 5')
-
+    print('K is not less than 3 anymore')
 

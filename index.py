4ece4ec282e2d8fc1e894df7269c28a01e77768b
Huu Hung <huuhungnguyen2002@gmail.com>
2023-05-16 11:34:59 +0700

    add file modifile

--- index.py
+++ index.py
@@ -1,47 +1,53 @@
-k = 5
-while k > 0:
-    print('K = ', k)
-    k -= 1
+# while
+k = 0
+while k < 5:
+    print("I am coding run", k)
+    k += 1
 
-s = 'How Kteam'
-idx = 0 # Vị trí bắt đầu muốn xử lý chuỗi
-length = len(s) # Lấy độ dài chuỗi làm mốc kết thúc
-while idx < length:
-    print(idx, 'stands for', s[idx])
+
+s = "Yushing"
+idx = 0
+length = len(s)
+
+while idx < length :
+    print(idx,"start for code run", s[idx])
     idx += 1
 
-# BREAK 
+print("=====")
+
+# Break
+
 five_even_numbers = []
 k_number = 1
 
+
+five_number = []
+y_number = 1
+
 while True:
-    if k_number % 2 == 0:
-        five_even_numbers.append(k_number)
-        if len(five_even_numbers) >= 5:
+    if y_number % 2 == 0:
+        five_number.append(y_number)
+        if len(five_number) >= 5:
             break
+    y_number += 1
+
+print(five_number)
+print(y_number)
+
+
+k_number = 0
+while k_number < 10:
     k_number += 1
-
-print(five_even_numbers)
-print(k_number)
-
-print("=======")
-
-# Continue
-k_number2 = 0
-while k_number2 < 10:
-    k_number2 += 1
-    if k_number2 % 2 == 0: # Nếu in ra những số chẵn thì nó sẽ bỏ qua và tiếp tục in 
+    if k_number % 2 == 0:
         continue
-    print(k_number2, "is odd number")
+    print('K is number odd', k_number)
 
-print(k_number2)
 
 k = 0
-while k < 3:
-    print('value key is of ',k)
+while k < 5:
+    print('Value key is of', k)
     k += 1
     if k == 2:
         break
-else: 
+else:
     print('K is not less than 3 anymore')
-

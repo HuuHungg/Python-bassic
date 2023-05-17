c5851eabfd8df97a79eb29d47912f8857307e144
Huu Hung <huuhungnguyen2002@gmail.com>
2023-05-17 15:06:35 +0700

    add file modifile

--- index.py
+++ index.py
@@ -35,3 +35,82 @@ f()
 f()
 f()
 f()
+
+
+
+
+def add_number(x,y):
+    return x + y
+
+print(add_number(2,3))
+
+
+def total_sum (a,b = 2, c = 3, d = 4):
+    f = (a + d ) * (b + c)
+    print(f)
+
+total_sum(1, d = 4)
+
+def yushing5(pos_or_key, key_arg_1, key_arg_2):
+    print(pos_or_key)
+    print(key_arg_1)
+    print(key_arg_2)
+
+yushing5(pos_or_key="毎日ITを勉強している", key_arg_1="２時間ぐらい", key_arg_2="難しいです")
+
+
+# Diện tích hình chữ nhật
+def recatangle_area(width, height):
+    return width * height
+area = recatangle_area(7,10)
+print(area)
+
+def print_info(name, age, gender):
+    print("Tên", name)
+    print("Tuổi", age)
+    print("Giới tính", gender)
+
+print('John', 21, "Nam")
+
+def find_max(numbers):
+    max_num = numbers[0]
+    for num in numbers:
+        if num > max_num:
+            max_num = num
+    return max_num
+
+reusult = find_max([1,2,3,4,5,6,7,8,9,10,11,12,13,15,16])
+print(reusult)
+
+
+def sum_list(numbers):
+    total = 0
+    for num in numbers:
+        total += num
+    return total
+
+result = sum_list([1,2,3,4,5,6])
+print(result)
+
+
+def is_prime(n):
+    if n in [2,3]:
+        return True
+    if (n == 1) or (n % 2 == 0):
+        return False
+    r = 3
+    while r * r <= n:
+        if n % r == 0:
+            return False
+        r += 2
+    return True
+print(is_prime(78), is_prime(79))
+
+def evenOdd(x):
+    if(x % 2 == 0):
+        print("event")
+    else:
+        print("odd")
+
+evenOdd(2)
+evenOdd(3)
